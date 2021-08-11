187fbc547986d7e9bb46628f01fc6559c794c78e
marquet <marquet@element84.com>
2021-08-11 13:42:23 -0400

    Bug Fix

--- sick-fits/backend/mutations/addToCart.ts
+++ sick-fits/backend/mutations/addToCart.ts
@@ -1,6 +1,6 @@
-/* eslint-disable  */
+/* eslint-disable */
 import { KeystoneContext, SessionStore } from '@keystone-next/types';
-import { CartItem } from '../schemas/CartItem'
+import { CartItem } from '../schemas/CartItem';
 import { Session } from '../types';
 
 import { CartItemCreateInput } from '../.keystone/schema-types';
@@ -11,8 +11,8 @@ async function addToCart(
   context: KeystoneContext
 ): Promise<CartItemCreateInput> {
   console.log('ADDING TO CART!');
-  const sesh = context.session as Session;
 
+  const sesh = context.session as Session;
   if (!sesh.itemId) {
     throw new Error('You must be logged in to do this!');
   }
@@ -23,7 +23,6 @@ async function addToCart(
   });
 
   const [existingCartItem] = allCartItems;
-
   if (existingCartItem) {
     console.log(
       `There are already ${existingCartItem.quantity}, increment by 1!`
@@ -31,14 +30,16 @@ async function addToCart(
     return await context.lists.CartItem.updateOne({
       id: existingCartItem.id,
       data: { quantity: existingCartItem.quantity + 1 },
-      resolveFields: false
+      resolveFields: false,
     });
   }
   return await context.lists.CartItem.createOne({
     data: {
       product: { connect: { id: productId } },
-      user: { connect: { id: sesh.itemId } }
+      user: { connect: { id: sesh.itemId } },
     },
-    resolveFields: false
+    resolveFields: false,
   })
 }
+
+export default addToCart;
